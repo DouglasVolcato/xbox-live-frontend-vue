f4bd84d916bb261ce637bbb72d198fe8b251d6f4
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-19 20:10:01 -0300

    feat: add RouteComposer helper

---
+++ src/main/vue-adapter/helpers/routeComposer-helper.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Teste</p>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { makeRouteComposerFactory } from "../../factories/routes/route-composer-factory";
+const router = useRouter();
+const routes = makeRouteComposerFactory();
+
+onMounted(() => {
+  routes.compose(setRoute);
+});
+
+function setRoute(route) {
+  router.push(route);
+}
+</script>
